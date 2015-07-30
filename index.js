66a390d8cc6027c142e82a9e84e5f55e0e1aba6e
Trang <trangtv04110@gmail.com>
2015-07-30 14:39:39 +0700

    Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app	 	= require('express')();
-var http 	= require('https').Server(app);
+var http 	= require('https').createServer(app);
 var io 		= require('socket.io')(http);
 
 app.get('/', function(req, res) {
